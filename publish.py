016c712ce6e28bf8c42d8639dfdc9662bfcc65b2
Dylan <meeusdylan@protonmail.com>
2020-07-12 20:09:31 +0200

    publish blog

--- publish.py
+++ publish.py
@@ -1,9 +1,10 @@
 import os
 
 if __name__ == '__main__':
+    blogloc = os.environ["GHBLOG"]
     os.system("hugo")
-    os.system("sudo rm -rf " + os.environ["GHBLOG"] + "*")
-    os.system("sudo cp -r public/* " + os.environ["GHBLOG"])
+    os.system("sudo rm -rf " + blogloc + "*")
+    os.system("sudo cp -r public/* " + blogloc)
     os.system("cd " + os.environ["GHBLOG"])
     os.system("git add .")
     os.system("git commit -m \"publish blog\"")
